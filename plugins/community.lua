1df6550b8bef18239557d580cce09551f4063da5
Mitja Belak <mico@0x0.icu>
2023-10-08 15:01:52 +0200

    Added typescript support

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,7 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
   { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.typescript" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   { 'Mofiqul/dracula.nvim'  },
 }
